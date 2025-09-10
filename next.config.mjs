d766d86740aa62d7bb7790a983b2b8371e080e82
ramosshan32-cloud <ramosshan32@gmail.com>
2025-09-10 23:06:59 +0800

    nothing

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+    manifest: '/manifest.json',
+  },
+});
+
+export default nextConfig;
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+// Set the default port for Next.js dev server to 8080
+module.exports = {
+  devServer: {
+    port: 8080,
+  },
+};
